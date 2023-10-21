cb391ebab0c9c868dbd7dc6af8ebb2b802db2c6b
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2023-10-21 11:25:04 +0100

    Chapter 14.5 and 14.6 added a constant chunk

---
+++ value.h
@@ -0,0 +1,23 @@
+//
+// Created by Swapnil Chauhan on 08/10/2023.
+//
+
+#ifndef CLOX_VALUE_H
+#define CLOX_VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* valueArray);
+void writeValueArray(ValueArray* valueArray, Value value);
+void freeValueArray(ValueArray* valueArray);
+void printValue(Value value);
+
+#endif //CLOX_VALUE_H
