4950143e9656a9642a3e98e8d963a83afe709d01
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2026-02-15 13:04:05 +0000

    Upto Chapter 19

--- value.c
+++ value.c
@@ -2,9 +2,12 @@
 // Created by Swapnil Chauhan on 08/10/2023.
 //
 
-#include "value.h"
-#include "memory.h"
 #include <stdio.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
 
 void initValueArray(ValueArray *array) {
   array->values = NULL;
@@ -39,6 +42,9 @@ void printValue(Value value) {
   case VAL_NUMBER:
     printf("%g", AS_NUMBER(value));
     break;
+  case VAL_OBJ:
+    printObject(value);
+    break;
   }
 }
 
@@ -52,6 +58,12 @@ bool valuesEqual(Value a, Value b) {
     return true;
   case VAL_NUMBER:
     return AS_NUMBER(a) == AS_NUMBER(b);
+  case VAL_OBJ: {
+    ObjString *aString = AS_STRING(a);
+    ObjString *bString = AS_STRING(b);
+    return aString->length == bString->length &&
+           memcmp(aString->chars, bString->chars, aString->length) == 0;
+  }
   default:
     return false; // Unreachable
   }
